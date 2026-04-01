4de88c8f4a5828f360c7e466afd2c0545b8fd784
Aaron Martin <martin.aaron55@gmail.com>
2026-04-01 17:47:24 -0500

    update schedule state to support undo (#2)

--- src/reducer/schedule.js
+++ src/reducer/schedule.js
@@ -4,32 +4,53 @@ import {
     DELETE_SCHEDULE_MEETING
 } from "../action/actionTypes.js";
 
-const initialState = [];
+const initialState = {
+    items: [],
+    prevItems: [],
+};
 
 function schedule(state = initialState, action) {
     switch (action.type) {
         case SCHEDULE_MEETING:
-            return [
-                ...state,
-                {
-                    ...action.payload
-                }
-            ];
+            return {
+               prevItems: [
+                   state.items.map((item) => ({...item})),
+                   ...state.prevItems
+               ],
+               items: [
+                    ...state.items,
+                    {
+                        ...action.payload
+                    }
+                ]
+            };
         case RESCHEDULE_MEETING:
-            return state.reduce((acc, entry) => {
-                if (entry.id === action.payload.scheduleId) {
-                    acc.push({
-                        ...entry,
-                        time: action.payload.time,
-                        day: action.payload.day
-                    });
-                } else {
-                    acc.push(entry);
-                }
-                return acc;
-            }, [])
+            return {
+                prevItems: [
+                    state.items.map((item) => ({...item})),
+                    ...state.prevItems
+                ],
+                items: state.items.reduce((acc, entry) => {
+                    if (entry.id === action.payload.scheduleId) {
+                        acc.push({
+                            ...entry,
+                            time: action.payload.time,
+                            day: action.payload.day
+                        });
+                    } else {
+                        acc.push(entry);
+                    }
+                    return acc;
+                }, [])
+            }
         case DELETE_SCHEDULE_MEETING:
-            return state.filter((entry) => (entry.id !== action.payload))
+            return {
+                prevItems: [
+                    state.items.map((item) => ({...item})),
+                    ...state.prevItems
+                ],
+                items: state.items.filter((entry) => (entry.id !== action.payload))
+            }
         default:
             return state;
     }
